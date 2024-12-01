f25700716bfe8fd532bfd5d054c965289f7a2028
Yewodev <yewogo2022@gmail.com>
2024-12-01 02:39:00 +0000

    products routes setup

---
+++ api/src/controller/products/controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express"
+
+
+export function listProducts(req:Request, res: Response){
+    res.send('the list of products 123')
+}
+
+export function getProductById(req:Request, res: Response){
+    res.send('get product by id')
+} 
+export function createProduct(req:Request, res: Response){
+    res.send('createProduct')
+} 
+
+export function updateProduct(req:Request, res: Response){
+    res.send('updateProduct')
+} 
+export function deleteProduct(req:Request, res: Response){
+    res.send('deleteProduct')
+} 
--- api/src/route/products/index.ts
+++ api/src/route/products/index.ts
@@ -1,19 +1,14 @@
 import { Router } from "express";
+import { listProducts, getProductById, createProduct, deleteProduct, updateProduct } from "../../controller/products/controller";
 
 //route for products endpoints
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('the list of products')
-});
+router.get('/',listProducts );
+router.get('/:id',getProductById);
+router.post('/',createProduct);
+router.put('/:id',updateProduct);
+router.delete('/:id',deleteProduct);
 
-router.get('/:id', (req, res) => {
-    console.log(req.params)
-    res.send('the list of products')
-});
-
-router.post('/',(req ,res)=>{
-    res.send('new product created')
-});
   
 export default router;
